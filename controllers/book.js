2080931a24831e23076539c09957bc22041c14dd
flotoine <florian.antoine.m@gmail.com>
2024-06-04 17:54:53 +0200

    Add one book functions

--- controllers/book.js
+++ controllers/book.js
@@ -5,3 +5,44 @@ exports.getAllBooks = (req, res, next) => {
       .then(books => res.status(200).json(books))
       .catch(error => res.status(400).json({ error }));
 };
+
+exports.getOneBook = (req, res, next) => {
+  Book.findOne({_id: req.params.id})
+  .then(book => res.status(200).json(book))
+  .catch(error => res.status(400).json({ error }));
+}
+
+exports.getBestBooks = (req, res, next) => {
+  
+}
+
+exports.addBook = (req, res, next) => {
+  const bookObject = JSON.parse(req.body.book);
+  const book = new Book ({
+    ...bookObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  })
+
+  book.save()
+    .then(() => res.status(201).json({message: "Livre enregistré !"}))
+    .catch(error => res.status(400).json({error}))
+}
+
+exports.editBook = (req, res, next) => {
+  Book.updateOne({ _id: req.params.id },{...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({message: "Livre modifié !"}))
+    .catch(error => res.status(400).json({error}))
+}
+
+exports.deleteBook = (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({message: "Livre supprimé !"}))
+    .catch(error => res.status(400).json({error}))
+}
+
+/*
+exports.rateBook = (req, res, next) => {
+  Book.addOne({ _id: req.params.id }, {})
+    .then(() => res.status(200).json({message: "Livre noté !"}))
+    .catch(error => res.status(400).json({error}))
+} */
--- routes/book.js
+++ routes/book.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth') //ajouter auth aux routes qui ont besoin
+const multer = require('../middleware/multer-config')
 
 const bookCtrl = require('../controllers/book')
 
-
+ /// ajout auth plus tard lorsque demandé
 router.get('/', bookCtrl.getAllBooks);
+router.get('/:id', bookCtrl.getOneBook);
+/// router.get('/bestrating', bookCtrl,bookCtrl.getBestBooks);
+router.post('/', multer, bookCtrl.addBook);
+router.put('/:id', bookCtrl.addBook); ///controlleur à revoir
+router.delete('/:id', bookCtrl.deleteBook);
+///router.post('/:id/rating', auth, bookCtrl.rateBook);
 
 module.exports = router;
