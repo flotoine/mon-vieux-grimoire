7c1c3a65db83852f81c3e86fb4ec709a7983bf20
flotoine <florian.antoine.m@gmail.com>
2024-06-20 17:36:41 +0200

    Sécurisation

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path'); 
+
+const helmet = require('helmet');
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "same-site" }
+}));
+
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
+
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+app.use(express.json());
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+module.exports = app;
